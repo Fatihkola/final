4bdc0115fcbc8b1a0b446130e4566421c96d2ce6
Fatihkola <73134805+Fatihkola@users.noreply.github.com>
2022-06-02 21:37:46 +0300

    final

---
+++ final/uyg05/uyg05/ViewModel/OdevModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace uyg05.ViewModel
+{
+    public class OdevModel
+    {
+        public string odevId { get; set; }
+        public string odevDersId { get; set; }
+        public string odevDersAdi{ get; set; }
+        public string odevKonu { get; set; }
+
+        public int odevOgrSayisi { get; set; }
+    }
+}
